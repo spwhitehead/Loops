e150155d922fc105e26bd1f09655078d19edb42e
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-02-23 11:36:41 -0700

    Initial commit for basic_math_functions.py

---
+++ basic_math_functions.py
@@ -0,0 +1,7 @@
+# Write a function add(x: float, y: float) -> float that adds two numbers.
+
+# Write a function multiply(x: float, y: float) -> float that multiplies two numbers.
+
+# Write a function square(x: float) -> float that squares a number (hint: it must use multiply).
+
+# Write a function add_squares(x: float, y: float) -> float that adds the squares of two numbers (hint: it must use both add and square).
