68796eb6a4a2d162b382a31ce27a87a4143619e0
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-02-23 11:53:45 -0700

    Adds logic for the basic math functions

--- basic_math_functions.py
+++ basic_math_functions.py
@@ -5,3 +5,29 @@
 # Write a function square(x: float) -> float that squares a number (hint: it must use multiply).
 
 # Write a function add_squares(x: float, y: float) -> float that adds the squares of two numbers (hint: it must use both add and square).
+
+x = 9
+y = 4
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+def multiply(x: float, y: float) -> float:
+    return x * y
+
+def square(x: float) -> float:
+    return multiply(x,x)
+
+def add_squares(x: float, y: float) -> float:
+    return add(multiply(x,x), multiply(y,y))
+
+
+def main():
+    print(add(x,y))
+    print(multiply(x,y))
+    print(square(x))
+    print(add_squares(x,y))
+
+
+if __name__ == "__main__":
+    main()
